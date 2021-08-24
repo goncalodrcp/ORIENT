88944bf7875e4dafcc0ad5c0f5aba4528b6fef00
Gonçalo Pereira <goncalo.c.pereira1@gmail.com>
2021-08-24 19:04:42 +0100

    generate figure for thesis report

--- MATLAB (main)/VisualUtilities/plotSphereTest.m
+++ MATLAB (main)/VisualUtilities/plotSphereTest.m
@@ -13,7 +13,7 @@ axis equal;
 %Define colors,lighting, etc
 sph.FaceColor = [0.53,0.81,0.93]; %Face color
 sph.FaceAlpha = 0.4; %Face transparency
-sph.EdgeAlpha = 0.6; %Edge transparency
+sph.EdgeAlpha = 0.3; %Edge transparency
 
 %Define axis' limits
 xlim([-1.5 1.5]);
@@ -28,7 +28,7 @@ hold on;
 %% Plot a plane on top of the sphere
 
 %Define vertices
-v = [-1 1 1; -1 -1 1; 1 -1 1; 1 1 1];
+v = [-1.5 1.5 1; -1.5 -1.5 1; 1.5 -1.5 1; 1.5 1.5 1];
 %Define faces
 f = [1 2 3 4];
 
@@ -39,4 +39,44 @@ pl = patch('Faces',f,'Vertices',v);
 pl.FaceColor = [0.46,0.96,0.62];
 pl.FaceAlpha = 0.6;
 pl.EdgeAlpha = 0;
+
+%% Plot points on tangent space and projection on the sphere
+
+%Define point at identity
+startPoint = [0 0 1]; 
+%in this case the point corresponds to the intersection between the sphere
+%and the tangent plane
+
+%Define rotation - To keep it simple, we consider only a rotation around
+%the X-Axis
+theta = 60; %deg
+%R = rotx(angle);
+dtheta = 2;
+v = 2:dtheta:theta;
+points = zeros(3,length(v)+1);
+points(:,1) = startPoint';
+for i=1:length(v)
+      angle = v(i);
+      R = rotx(angle);
+      points(:,i+1) = R*startPoint';
+end
+
+endPoint = points(:,end);
+
+%Plot 3d points and surface line
+%Plot identity point
+plot3(startPoint(1),startPoint(2),startPoint(3),'X','Color','r','LineWidth',2);
+text(0,0,1.2,'$g$','interpreter','latex','FontSize',14,'FontWeight','bold');
+%Plot another point in the Lie group
+plot3(endPoint(1),endPoint(2),endPoint(3),'.','Color','r','MarkerSize',10);
+text(endPoint(1),endPoint(2)-0.2,endPoint(3),'$g^{\prime}$','interpreter','latex','FontSize',14,'FontWeight','bold');
+plot3(points(1,2:end-1),points(2,2:end-1),points(3,2:end-1),'-','Color','r','LineWidth',1.5);
+%Plot arbitrary point in tangent space
+plot3([startPoint(1) endPoint(1)],[startPoint(2) endPoint(2)],[1 1],'-','Color','#0072BD','LineWidth',1.5);
+plot3(endPoint(1),endPoint(2),1,'.','Color','#0072BD','MarkerSize',10);
+%text(endPoint(1),endPoint(2),1.2,'$\frak{g}$','interpreter','latex','FontSize',14,'FontWeight','bold');
+text(-0.8,0.8,1.2,'$T_{g}\mathcal{G}$','interpreter','latex','FontSize',14,'FontWeight','bold');
+set(gca,'visible','off')
+print(gcf,'foo.png','-dpng','-r300');
+
 
